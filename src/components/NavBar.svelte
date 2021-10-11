60ee0f6d24d998d089500b30a26043f188d2ab1d
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-11 15:10:44 -0400

    Add NavBar component

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,39 @@
+<div class="container">
+  <nav class="navbar">
+    <ol class="nav-links">
+      <li class="link"><a>About</a></li>
+      <li class="link"><a>Skills</a></li>
+      <li class="link"><a>Projects</a></li>
+      <li class="link"><a>Contact</a></li>
+    </ol>
+  </nav>
+</div>
+
+<style>
+  .container {
+    height: 100px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  ol {
+    font-family: var(--font-mono);
+    font-size: 0.8rem;
+    display: flex;
+    justify-content: flex-end;
+    counter-reset: link 0;
+  }
+
+  ol li {
+    color: #dfdfdf;
+    padding: 10px;
+  }
+  
+  ol li a::before {
+    counter-increment: link;
+    content: "0" counter(link) ".";
+    color: var(--accent);
+    margin-right: 5px;
+  }
+</style>
