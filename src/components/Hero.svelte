e1a1e54aaaa08397027549fb4502b0c223e71741
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-11 15:10:15 -0400

    Add Hero component

---
+++ src/components/Hero.svelte
@@ -0,0 +1,34 @@
+
+<section class="hero">
+  <h1>Greetings, my name is</h1>
+	<h2>Christian Bolus</h2>
+	<p>I'm a software engineer speicializing in technologies for the web.</p>
+</section>
+
+<style>
+	.hero {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+
+	}
+  h1 {
+		color: var(--accent);
+		font-family: var(--font-mono);
+		font-size: 1em;
+		font-weight: 100;
+		margin: 0;
+	}
+
+	h2 {
+    color: #dfdfdf;
+		font-size: 4rem;
+    font-weight: 700;
+		margin: 0;
+	}
+
+  p {
+    font-size: 1.1rem;
+    color: #fff;
+  }
+</style>
