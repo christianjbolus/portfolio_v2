f0a38bc46096d4e28ca68616cd7b1004f1dad25b
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-11 15:09:48 -0400

    Add App component

---
+++ src/components/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Hero from './Hero.svelte'
+	import NavBar from './NavBar.svelte'
+</script>
+
+<main>
+	<NavBar />
+	<Hero />
+</main>
+
+<style>
+	main {
+		padding: 0 100px;
+	}
+</style>
