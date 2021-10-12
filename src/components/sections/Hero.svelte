c8de0dfbcc39398822808e1b0e86cb83d2aa809a
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-12 18:28:06 -0400

    Store hero elements in state and render with each block

--- src/components/sections/Hero.svelte
+++ src/components/sections/Hero.svelte
@@ -1,19 +1,19 @@
 <script>
 	import Transition from '../Transition.svelte'
+
+	const one = `<h1>Greetings, my name is</h1>`
+	const two = `<h2>Christian Bolus</h2>`
+	const three = `<p>I'm a software engineer specializing in technologies for the web.</p>`
+	const items = [one, two, three]
 	
 </script>
+
 <section class="hero">
-	<Transition config={{y: -100, duration: 500, delay: 200}}>
-		<h1>Greetings, my name is</h1>
-	</Transition>
-	<Transition config={{y: -100, duration: 500, delay: 100}}>
-		<h2>Christian Bolus</h2>
-	</Transition>
-	<Transition config={{y: -100, duration: 500}}>
-		<p>
-			I'm a software engineer specializing in technologies for the web.
-		</p>
-	</Transition>
+	{#each items as item, i}
+		<Transition config={{y: -100, duration: 500, delay: ((items.length * 100) - (i * 100))}}>
+			<div>{@html item}</div>
+		</Transition>
+	{/each}
 </section>
 
 <style>
@@ -26,7 +26,7 @@
 
 	}
 
-  h1 {
+  div :global(h1) {
 		color: var(--accent);
 		font-family: var(--font-mono);
 		font-size: 1em;
@@ -34,14 +34,14 @@
 		margin: 0 0 20px 4px;
 	}
 
-	h2 {
+	div :global(h2) {
     color: #dfdfdf;
 		font-size: 4rem;
     font-weight: 700;
 		margin-bottom: 20px;
 	}
 
-  p {
+  div :global(p) {
     font-size: 1.1rem;
     color: rgb(165, 165, 165);
   }
