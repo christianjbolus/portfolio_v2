ad8dc9cc38a8a2e9139c44c49a058032d06d9ff3
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-12 12:28:21 -0400

    Add Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let text;
+</script>
+
+<a href="https://drive.google.com/file/d/1EKgM_wVM-yNtksujrNpQMe5cnDGQxavg/view?usp=sharing">{text}</a>
+
+<style>
+  a {
+    color: var(--accent);
+    font-family: var(--font-mono);
+    text-decoration: none;
+    border: 1px solid var(--accent);
+    border-radius: 5px;
+    padding: 12px 16px;
+    background-color: transparent;
+    margin-left: 20px;
+    cursor: pointer;
+  }
+
+  a:hover {
+    background-color: var(--red-tint)
+  }
+</style>
