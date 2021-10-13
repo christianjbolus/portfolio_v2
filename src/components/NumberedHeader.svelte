27a86c40c7fe09fadcc65f3b0705d6ffd6130d3d
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-13 17:54:25 -0400

    Add initial styling to NumberedHeader

--- src/components/NumberedHeader.svelte
+++ src/components/NumberedHeader.svelte
@@ -1,12 +1,36 @@
 <script>
-  export let number
   export let text
 </script>
 
-<h2><span>{number}</span>{text}</h2>
+<h2>{text}</h2>
 
-<style>
+<style lang="scss">
   h2 {
-    color: var(--light-grey)
+    color: var(--light-grey);
+    display: flex;
+    align-items: center;
+    font-size: 30px;
+    font-weight: 600;
+    margin-bottom: 40px;
+
+    &:before {
+      content: "01.";
+      color: var(--accent);
+      font-family: var(--font-mono);
+      position: relative;
+      top: 3px;
+      font-size: 24px;
+      font-weight: 400;
+      margin-right: 10px;
+    }
+
+    &:after {
+      content: "";
+      display: block;
+      width: 300px;
+      height: 1px;
+      margin-left: 20px;
+      background-color: var(--dark-grey)
+    }
   }
 </style>
