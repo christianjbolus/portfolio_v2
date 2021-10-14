fb4b88d9d470b086af6f3e8f2524080215fceb32
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-14 17:59:43 -0400

    Add data.js file to store page data used in components

---
+++ src/data.js
@@ -0,0 +1,41 @@
+module.exports = {
+  email: 'christianjbolus@gmail.com',
+
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/christianjbolus'
+    },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/christianbolus/',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/ChristianJBolus'
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/christianbolus/'
+    }
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about'
+    },
+    {
+      name: 'Skills',
+      url: '/#skills'
+    },
+    {
+      name: 'Projects',
+      url: '/#projects'
+    },
+    {
+      name: 'Contact',
+      url: '/#contact'
+    }
+  ]
+}
