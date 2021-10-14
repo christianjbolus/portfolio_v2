9673d78c2b368c9492d2eb7cf21dbe70d855ccb4
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-14 18:29:48 -0400

    Add Icon component to dynamically render socila media icons

---
+++ src/components/icons/Icon.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { GitHub, LinkedIn, Twitter, Instagram } from './index'
+  export let name;
+</script>
+
+{#if name === 'GitHub'}
+  <GitHub />
+{:else if name === 'LinkedIn'}
+  <LinkedIn />
+{:else if name === 'Twitter'}
+  <Twitter />
+{:else if name === 'Instagram'}
+  <Instagram />
+{/if}
