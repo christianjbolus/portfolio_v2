b9ad5fe4ab0c34e760d8f4226e9aa472e708cc64
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-14 10:28:20 -0400

    Add index file to icons directory for concise imports

---
+++ src/components/icons/index.js
@@ -0,0 +1,11 @@
+import GitHub from './Github.svelte'
+import LinkedIn from './LinkedIn.svelte'
+import Twitter from './Twitter.svelte'
+import Instagram from './Instagram.svelte'
+
+export {
+  GitHub,
+  LinkedIn,
+  Twitter,
+  Instagram
+}
