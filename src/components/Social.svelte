96613851967886638bcd1b975883a1a3f650e648
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-14 18:02:20 -0400

    Add Social component to display social media icons

--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -1,6 +1,29 @@
 <script>
-  import GitHub from './icons/github.svelte'
-  import LinkedIn from './icons/LinkedIn.svelte'
-  import Twitter from './icons/Twitter'
-  import Instagram from './icons/Instagram.svelte'
+  import { GitHub, LinkedIn, Twitter, Instagram } from './icons/index'
+  const icons = [GitHub, LinkedIn, Twitter, Instagram]
 </script>
+
+<div class="social">
+  <a href="https://github.com/christianjbolus">
+    <GitHub />
+  </a>
+  <a>
+    <LinkedIn />
+  </a>
+  <a>
+    <Twitter />
+  </a>
+  <a>
+    <Instagram />
+  </a>
+</div>
+
+<style lang="scss">
+  .social {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    bottom: 0;
+    left: 20px;
+  }
+</style>
