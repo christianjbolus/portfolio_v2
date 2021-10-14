a7ce3e7878b801359518f16bed9f07ada69c250a
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-13 20:58:49 -0400

    Add Side and Social component shells

---
+++ src/components/Social.svelte
@@ -0,0 +1,6 @@
+<script>
+  import GitHub from './icons/github.svelte'
+  import LinkedIn from './icons/LinkedIn.svelte'
+  import Twitter from './icons/Twitter'
+  import Instagram from './icons/Instagram.svelte'
+</script>
