ef38fd5e62c9b9098103044642c22c499c3c1e17
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-16 12:50:54 -0400

    Add scrollreveal config to data.js

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/christianjbolus'
+      url: 'https://github.com/christianjbolus',
     },
     {
       name: 'LinkedIn',
@@ -12,30 +12,49 @@ module.exports = {
     },
     {
       name: 'Twitter',
-      url: 'https://twitter.com/ChristianJBolus'
+      url: 'https://twitter.com/ChristianJBolus',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/christianbolus/'
-    }
+      url: 'https://www.instagram.com/christianbolus/',
+    },
   ],
 
   navLinks: [
     {
       name: 'About',
-      url: '/#about'
+      url: '/#about',
     },
     {
       name: 'Skills',
-      url: '/#skills'
+      url: '/#skills',
     },
     {
       name: 'Projects',
-      url: '/#projects'
+      url: '/#projects',
     },
     {
       name: 'Contact',
-      url: '/#contact'
-    }
-  ]
-}
+      url: '/#contact',
+    },
+  ],
+
+  srConfig: {
+    delay: 300,
+    distance: '40px',
+    duration: 600,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    interval: 0,
+    opacity: 0,
+    origin: 'bottom',
+    rotate: { x: 0, y: 0, z: 0 },
+    scale: 1,
+    cleanup: false,
+    desktop: true,
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  },
+};
