a8176671b426b90d26361621c74f9dd33ef1fc41
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-16 12:52:05 -0400

    Add ScrollReveal effect wrapper component

---
+++ src/components/effects/ScrollReveal.svelte
@@ -0,0 +1,12 @@
+<script>
+  import ScrollReveal from 'scrollreveal'
+  import { srConfig } from '../../data'
+
+  function scrollReveal(node) {
+    ScrollReveal().reveal(node, srConfig)
+  }
+</script>
+
+<div class="revealer" use:scrollReveal>
+  <slot />
+</div>
