a7f2f70a84a5e184089ecaff13946cb085195598
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-16 12:52:51 -0400

    Wrap About section in ScrollReveal component

--- src/components/sections/About.svelte
+++ src/components/sections/About.svelte
@@ -1,35 +1,37 @@
 <script>
   import NumberedHeader from "../NumberedHeader.svelte";
-  const skills = [
-    "JavaScript", "TypeScript", "Ruby", "HTML/CSS", "React", "Next.js", "Node.js", 
-    "Express", "Ruby on Rails", "Mongo DB", "PostgreSQL", "GraphQL", "Git"  
-    ]
+  import ScrollReveal from '../effects/ScrollReveal.svelte'
+  // const skills = [
+  //   "JavaScript", "TypeScript", "Ruby", "HTML/CSS", "React", "Next.js", "Node.js", 
+  //   "Express", "Ruby on Rails", "Mongo DB", "PostgreSQL", "GraphQL", "Git"  
+  //   ]
 </script>
-
-<section class="about" id="about">
-  <NumberedHeader text="About Me" />
-  <div class="container">
-    <div>
-      <p>
-        Hello there! Hard working and diligent Software Engineer with a love of learning and an insatiable curiosity.  
-        I have a tenacious approach to problem solving and am motivated by overcoming challenges and 
-        developing efficient solutions.  I’m passionate about building products that reduce friction 
-        in the lives of the user.    
-      </p>
-      <p>
-        My background in marketing allows me to view software through a user 
-        focused lens. This informs my design decisions from the perspective of the end user and my 
-        fellow developers.
-      </p>
-      <p>
-        As a fast learner who is able to get along with all personality types, 
-        I’m energized by working with high performance teams that are continually pushing their 
-        members to improve and reach their goals.
-      </p>
-    </div>
-    <img src="headshot.jpg" alt="Headshot"/>
-  </div>
-</section>
+  <ScrollReveal>
+    <section class="about">
+      <NumberedHeader text="About Me" />
+      <div class="container">
+        <div>
+          <p>
+            Hello there! Hard working and diligent Software Engineer with a love of learning and an insatiable curiosity.  
+            I have a tenacious approach to problem solving and am motivated by overcoming challenges and 
+            developing efficient solutions.  I’m passionate about building products that reduce friction 
+            in the lives of the user.    
+          </p>
+          <p>
+            My background in marketing allows me to view software through a user 
+            focused lens. This informs my design decisions from the perspective of the end user and my 
+            fellow developers.
+          </p>
+          <p>
+            As a fast learner who is able to get along with all personality types, 
+            I’m energized by working with high performance teams that are continually pushing their 
+            members to improve and reach their goals.
+          </p>
+        </div>
+        <img src="headshot.jpg" alt="Headshot"/>
+      </div>
+    </section>
+  </ScrollReveal>
 
 <style>
   .about {
