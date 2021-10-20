dcddc85badf472dcfd2d1c4c0e8efff72e776fef
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-19 21:12:43 -0400

    Add Phone component

---
+++ src/components/Phone.svelte
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<div class="phone-graphic">
+  <div class="phone-screen">
+    <div class="screen-display" id="neuze-img"></div>
+  </div>
+</div>
+
+<style>
+.phone-graphic {
+  height: 533px;
+  width: 264px;
+  background-color: rgb(85, 84, 84);
+  border-radius: 36px;
+  padding: 8px;
+  margin-bottom: 50px;
+  box-shadow: 0 20px 30px rgba(0, 0, 0, 0.4),
+              inset 0 -2px 8px rgba(0, 0, 0, 1);
+}
+
+.phone-screen {
+  background-color: rgb(26, 26, 26);
+  border-radius: 34px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.screen-display {
+  background-size: cover;
+  width: 100%;
+  height: 90%;
+  overflow: hidden;
+}
+</style>
