ef0affa4e8aee8fddbe4169864a5ab702ba994b6
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-19 22:22:25 -0400

    Implement grid layout for Project component

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -4,10 +4,26 @@
 
 <div class="container">
   <Phone />
+  <div class="description">
+    <p>Chatter is a full-stack micro blogging application with user auth that allows 
+      users to post 280 character “Chats.” A user can edit their Chats and respond to 
+      another user’s Chats. Includes form validation on register/login forms. 
+      Built with a Ruby on Rails/PostgreSQL backend and a React/Next.js frontend.</p>
+  </div>
 </div>
 
 <style>
   .container {
     display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    gap: 10px;
   }
+
+  .description {
+    background-color: #fff;
+    grid-column: 4 / 13;
+    grid-row: 1 / 1;
+    align-self: center;
+  }
+
 </style>
