b3eb16b932814675aa6da20b668b1a1d5d4ef65f
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-20 20:24:46 -0400

    Add project data to data.js

--- src/data.js
+++ src/data.js
@@ -39,6 +39,39 @@ module.exports = {
     },
   ],
 
+  projects: [
+    {
+      name: 'Chatter',
+      description:
+        'Chatter is a full-stack micro blogging application with user auth that allows users to post 280 character “Chats.” A user can edit their Chats and respond to another user’s Chats. Includes form validation on register/login forms. Built with a Ruby on Rails/PostgreSQL backend and a React/Next.js frontend.',
+        image: "chatter_main.png",
+        alt: "Chatter's main mobile view.",
+        tech: ["React", "Next.js", "Ruby", "Ruby on Rails", "PostgreSQL"],
+        url: "https://chatter-social.netlify.app/",
+        github: "https://github.com/christianjbolus/chatter"
+    },
+    {
+      name: 'My Table',
+      description:
+        'My Table is a full-stack application with user auth that allows users to Create, Read, Update and Delete their favorites recipes. It utilizes a custom authentication server built with Express and TypeScript and uses Airtable as the database. The frontend was built using React.',
+        image: "#",
+        alt: "My Table's main mobile view.",
+        tech: ["React", "Node.js", "Express", "TypeScript"],
+        url: "https://my-table.netlify.app/",
+        github: "https://github.com/christianjbolus/my_table"
+    },
+    {
+      name: 'Neuze',
+      description:
+        'Neuze is a frontend application utilizing the NY Times API that allows users to search for articles, view the details of an article using a custom built modal and save articles to their local storage for later viewing. Includes form validation on the search bar. Built using HTML, CSS and JavaScript.',
+        image: "#",
+        alt: "Neuze's main mobile view.",
+        tech: ["HTML", "CSS", "JavaScript"],
+        url: "https://christianjbolus.github.io/neuze/",
+        github: "https://github.com/christianjbolus/neuze"
+    },
+  ],
+
   srConfig: {
     delay: 300,
     distance: '40px',
