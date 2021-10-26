8f326fbc78d3c0c1411b652876fe6a967ad6a37b
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-26 18:34:21 -0400

    Add initial content to Contact section component

--- src/components/sections/Contact.svelte
+++ src/components/sections/Contact.svelte
@@ -1,8 +1,35 @@
 <script>
-  import { NumberedHeader } from '../index'
   import { ScrollReveal } from '../effects/index'
 </script>
 
 <ScrollReveal>
-  <NumberedHeader text="Contact" />
+  <section class="contact" id="contact">
+    <h2>Like What You See?</h2>
+    <h3>Get In Touch</h3>
+  </section>
 </ScrollReveal>
+
+<style lang="scss">
+  .contact {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    h2 {
+      counter-increment: section;
+      color: var(--accent);
+      font-family: var(--font-mono);
+  
+      &::before {
+        content: "0" counter(section) ".";
+        margin-right: 10px;
+      }
+    }
+    h3 {
+      color: var(--light-grey);
+      font-size: 3rem;
+    }
+
+  }
+
+</style>
