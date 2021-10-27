a0a720f861d42ce72af004dd84c3f457776b4390
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-27 15:16:56 -0400

    Add Layout component

---
+++ src/components/Layout.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { NavBar, Social, Email } from './index'
+</script>
+
+<div class="layout">
+  <NavBar />
+  <Social />
+  <Email />
+  <slot />
+</div>
+
+<style lang="scss">
+  .layout {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 0 150px;
+
+    @media (max-width: 1024px) {
+			padding: 0 100px;
+		}
+  }
+</style>
