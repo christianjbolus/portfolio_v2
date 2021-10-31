7cb2e4d00524d5397e3514d22ee242cf622e9d18
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-10-31 18:19:03 -0400

    Add default value to onClick prop in Button component

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,5 +1,5 @@
 <script>
-  export let text, url, className, onClick;
+  export let text, url, className, onClick = null;
 </script>
 
 <a class={className} href={url} on:click={onClick} target="_blank">{text}</a>
