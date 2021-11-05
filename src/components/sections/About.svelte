b6f7ec3ebe27e5228c8152c78d76de0412f54301
christianjbolus <59423548+christianjbolus@users.noreply.github.com>
2021-11-05 08:47:28 -0400

    Fix typo in About section

--- src/components/sections/About.svelte
+++ src/components/sections/About.svelte
@@ -11,7 +11,7 @@
       <div class="container">
         <div>
           <p>
-            Hello there!  My name is Christian and I'm passionate about build software products 
+            Hello there!  My name is Christian and I'm passionate about building software products 
             that reduce friction in the lives of users.  I have a tenacious approach to problem 
             solving and am motivated by overcoming challenges and developing efficient solutions.    
           </p>
@@ -24,7 +24,7 @@
             I’m energized by working with high performance teams that are continually pushing their 
             members to improve and reach their goals.
           </p>
-          <p>Some of the technologies I've been enjoying recently are:</p>
+          <p>Some of the technologies I've been using recently are:</p>
           <ul class="skill-list">
             {#each skills as skill }
               <li>{skill}</li>
